63308849b31665ab1320b863781f0274bf5165fb
Alym62 <gasafm62@gmail.com>
2024-04-17 10:05:31 -0300

    Teste unitário do método de salvar feito.

--- src/test/kotlin/org/me/crud/services/TaskServiceTest.kt
+++ src/test/kotlin/org/me/crud/services/TaskServiceTest.kt
@@ -2,14 +2,17 @@ package org.me.crud.services
 
 import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Assertions.assertNotNull
+import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.DisplayName
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.extension.ExtendWith
 import org.me.crud.domain.TaskEntity
+import org.me.crud.payloads.request.TaskDTORequest
 import org.me.crud.payloads.response.TaskDTOResponse
 import org.me.crud.repositories.TaskRepository
-import org.mockito.Mockito.mock
-import org.mockito.Mockito.`when`
+import org.mockito.Mock
+import org.mockito.Mockito.*
+import org.mockito.MockitoAnnotations
 import org.mockito.junit.jupiter.MockitoExtension
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.test.context.ActiveProfiles
@@ -19,8 +22,13 @@ import java.util.*
 @ActiveProfiles("test")
 @ExtendWith(MockitoExtension::class)
 class TaskServiceTest {
-    private val taskRepository = mock(TaskRepository::class.java)
-    private val taskService = TaskService(taskRepository)
+    @Mock
+    private lateinit var taskRepository: TaskRepository
+
+    @BeforeEach
+    fun setUp() {
+        MockitoAnnotations.openMocks(this)
+    }
 
     @Test
     @DisplayName("Test unit service - FindAll")
@@ -32,6 +40,7 @@ class TaskServiceTest {
 
         `when`(taskRepository.findAll()).thenReturn(tasksList)
 
+        val taskService = TaskService(taskRepository)
         val taskResponse = taskService.findAll()
 
         assert(taskResponse.size == tasksList.size)
@@ -48,9 +57,30 @@ class TaskServiceTest {
 
         `when`(taskRepository.findById(taskId)).thenReturn(Optional.of(taskEntity))
 
+        val taskService = TaskService(taskRepository)
+
         val taskResponse = taskService.findById(taskId)
         val expected = TaskDTOResponse(taskId, taskEntity.name, taskEntity.description, taskEntity.done)
 
         assertEquals(taskResponse, expected)
+    }
+
+    @Test
+    @DisplayName("Test unit service - Insert")
+    fun testInsert() {
+        val taskDTO = TaskDTORequest(name = "Task one", description = "Description one", done = true)
+        val taskEntity = TaskEntity(id = null, name = taskDTO.name, description = taskDTO.description, done = taskDTO.done)
+        val taskResponseRepository = TaskEntity(id = 1L, name = taskDTO.name, description = taskDTO.description, done = taskDTO.done)
+
+        `when`(taskRepository.save(taskEntity)).thenReturn(taskResponseRepository)
+
+        val taskService = TaskService(taskRepository)
+        val taskResponse = taskService.insert(taskDTO)
+        val expected = TaskDTOResponse(taskResponseRepository.id!!, taskResponseRepository.name, taskResponseRepository.description, taskResponseRepository.done)
+
+        assertEquals(expected.id, taskResponse.id)
+        assertEquals(expected.name, taskResponse.name)
+        assertEquals(expected.description, taskResponse.description)
+        assertEquals(expected.done, taskResponse.done)
     }
 }
