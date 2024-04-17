c4f8986a67cb91da129bca0b7b0830eb291be2c2
Alym62 <gasafm62@gmail.com>
2024-04-17 00:35:00 -0300

    Testes unitários iniciados. Teste findAll e findById feito.

---
+++ src/test/kotlin/org/me/crud/services/TaskServiceTest.kt
@@ -0,0 +1,56 @@
+package org.me.crud.services
+
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Assertions.assertNotNull
+import org.junit.jupiter.api.DisplayName
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.extension.ExtendWith
+import org.me.crud.domain.TaskEntity
+import org.me.crud.payloads.response.TaskDTOResponse
+import org.me.crud.repositories.TaskRepository
+import org.mockito.Mockito.mock
+import org.mockito.Mockito.`when`
+import org.mockito.junit.jupiter.MockitoExtension
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.context.ActiveProfiles
+import java.util.*
+
+@SpringBootTest
+@ActiveProfiles("test")
+@ExtendWith(MockitoExtension::class)
+class TaskServiceTest {
+    private val taskRepository = mock(TaskRepository::class.java)
+    private val taskService = TaskService(taskRepository)
+
+    @Test
+    @DisplayName("Test unit service - FindAll")
+    fun testGetAllTasks() {
+        val tasksList = listOf(
+            TaskEntity(1L, "Task one", "Description one", false),
+            TaskEntity(2L, "Task two", "Description two", false)
+        )
+
+        `when`(taskRepository.findAll()).thenReturn(tasksList)
+
+        val taskResponse = taskService.findAll()
+
+        assert(taskResponse.size == tasksList.size)
+        assert(taskResponse[0].name == tasksList[0].name)
+        assert(taskResponse[0].description == tasksList[0].description)
+        assertNotNull(taskResponse)
+    }
+
+    @Test
+    @DisplayName("Test unit service - FindById")
+    fun testGetById() {
+        val taskId = 1L
+        val taskEntity = TaskEntity(taskId, "Task one", "Description one", true)
+
+        `when`(taskRepository.findById(taskId)).thenReturn(Optional.of(taskEntity))
+
+        val taskResponse = taskService.findById(taskId)
+        val expected = TaskDTOResponse(taskId, taskEntity.name, taskEntity.description, taskEntity.done)
+
+        assertEquals(taskResponse, expected)
+    }
+}
